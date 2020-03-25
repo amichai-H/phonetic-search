d8fc78865e48d64868176741f7875032eddd387f
amichai-H <51719573+amichai-H@users.noreply.github.com>
2020-03-25 15:08:20 +0200

    Update PhoneticFinder.cpp

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -185,7 +185,7 @@ namespace phonetic{
             }
             
         }
-        std::string errorMessage = std::string("Did not find the word '")+s2+("' in the text .")+s1+(".");
+        std::string errorMessage = std::string("Did not find the word '")+s2+("' in the text");
 	    throw std::runtime_error(errorMessage);
     }
 };
